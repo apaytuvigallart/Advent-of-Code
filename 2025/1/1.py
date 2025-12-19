45b7fca5e1ac7a5cc32f2dd548ff74f860ff757f
Aleix Paytuví Gallart <aleixpaytuvigallart@MacBook-Pro-de-Aleix.local>
2025-12-19 08:39:54 +0100

    feat/2025-1

---
+++ 2025/1/1.py
@@ -0,0 +1,18 @@
+dial = 50
+password = 0
+
+with open("input.txt") as f:
+    for line in f:
+        line = line.strip()
+        direction = line[0]
+        steps = int(line[1:])
+
+        if direction == "R":
+            dial = (dial + steps) % 100
+        else:
+            dial = (dial - steps) % 100
+
+        if dial == 0:
+            password += 1
+
+print(password)
