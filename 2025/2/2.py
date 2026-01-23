0fd934cd61cc992e9ffdf93c3a5bfc089e4039c8
Aleix Paytuví Gallart <aleixpaytuvigallart@MacBook-Pro-de-Aleix.local>
2026-01-23 16:17:03 +0100

    feat: 2 aoc

---
+++ 2025/2/2.py
@@ -0,0 +1,15 @@
+result = []
+with open("input.txt") as f:
+    for line in f:
+        line = line.strip()
+        left = int(line.split("-")[0])
+        right = int(line.split("-")[1])
+
+        while left <= right:
+            s = str(left)
+            first, second = s[: len(s) // 2], s[len(s) // 2 :]
+            if first == second:
+                result.append(left)
+            left += 1
+
+print(sum(result))
