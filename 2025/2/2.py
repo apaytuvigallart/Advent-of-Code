59b679197abf2d2be4dd1bc76612b544c917d556
Aleix Paytuví Gallart <aleixpaytuvigallart@MacBook-Pro-de-Aleix.local>
2026-02-03 15:27:56 +0100

    feat: aoc 3

--- 2025/2/2.py
+++ 2025/2/2.py
@@ -4,12 +4,10 @@ with open("input.txt") as f:
         line = line.strip()
         left = int(line.split("-")[0])
         right = int(line.split("-")[1])
-
         while left <= right:
             s = str(left)
             first, second = s[: len(s) // 2], s[len(s) // 2 :]
             if first == second:
                 result.append(left)
             left += 1
-
 print(sum(result))
